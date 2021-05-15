7bb27d4d7eaf3e33a983a5526c3bf427b077179c
ShinyAkko <929417368@qq.com>
2021-05-15 16:36:15 +0800

    显示数据库

---
+++ Ledger/Ledger/Models/Record.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ledger.Models
+{
+    [SQLite.Table("works")]
+    public class Record {
+        /// <summary>
+        /// 主键
+        /// </summary>
+        [SQLite.Column("id")]
+        public int Id { get; set; }
+
+        /// <summary>
+        /// 内容
+        /// </summary>
+        [SQLite.Column("content")]
+        public string Content { get; set; }
+
+        /// <summary>
+        /// 类型
+        /// </summary>
+        [SQLite.Column("type")]
+        public string Type { get; set; }
+
+        /// <summary>
+        /// 金额
+        /// </summary>
+        [SQLite.Column("amount")]
+        public float Amount { get; set; }
+
+        /// <summary>
+        /// 年
+        /// </summary>
+        [SQLite.Column("year")]
+        public int Year { get; set; }
+
+        /// <summary>
+        /// 月
+        /// </summary>
+        [SQLite.Column("month")]
+        public int Month { get; set; }
+
+        /// <summary>
+        /// 日
+        /// </summary>
+        [SQLite.Column("day")]
+        public int Day { get; set; }
+
+        /// <summary>
+        /// 日
+        /// </summary>
+        [SQLite.Column("budget")]
+        public string Budget { get; set; }
+
+        private string _color;
+
+        /// <summary>
+        /// 颜色
+        /// </summary>
+        [SQLite.Ignore]
+        public string Color =>
+            _color ?? (_color = Budget == "收入" ? "green" : "red");
+
+        public const string Budgetexpense = "expense";
+
+        public const string Budgetincome = "income";
+    }
+}
---
+++ Ledger/Ledger/ViewModels/DetailPageViewModel.cs
@@ -0,0 +1,142 @@
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using System;
+using Ledger.Services;
+using Ledger.Models;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using Xamarin.Forms.Extended;
+
+namespace Ledger.ViewModels {
+    public class DetailPageViewModel : ViewModelBase {
+        private IRecordStorage _recordStorage;
+
+        public DetailPageViewModel(IRecordStorage recordStorage) {
+            _recordStorage = recordStorage;
+
+            RecordCollection = new InfiniteScrollCollection<Record>();
+            RecordCollection.OnCanLoadMore = () => _canLoadMore;
+            RecordCollection.OnLoadMore = async () => {
+                Status = Loading;
+                var records = await recordStorage.GetRecordsAsync(Where,
+                    RecordCollection.Count, PageSize);
+                Status = String.Empty;
+
+                if (records.Count < PageSize)
+                {
+                    _canLoadMore = false;
+                    Status = NO_MORE_RESULT;
+                }
+
+                if (RecordCollection.Count == 0 && records.Count == 0)
+                {
+                    Status = NO_RESULT;
+                }
+
+                return records;
+            };
+        }
+
+        // ******** 公开变量
+
+        /// <summary>
+        /// 加载状态。
+        /// </summary>
+        public string Status
+        {
+            get => _status;
+            set => Set(nameof(Status), ref _status, value);
+        }
+
+        /// <summary>
+        /// 加载状态。
+        /// </summary>
+        private string _status;
+
+        /// <summary>
+        /// 查询语句。
+        /// </summary>
+        public Expression<Func<Record, bool>> Where
+        {
+            get => _where;
+            set
+            {
+                Set(nameof(Where), ref _where, value);
+                _newQuery = true;
+            }
+        }
+
+        /// <summary>
+        /// 查询语句。
+        /// </summary>
+        private Expression<Func<Record, bool>> _where;
+
+        public InfiniteScrollCollection<Record> RecordCollection { get; }
+
+        /// <summary>
+        /// 页面显示命令
+        /// </summary>
+        private RelayCommand _pageAppearingCommand;
+
+        /// <summary>
+        /// 页面显示命令
+        /// </summary>
+        public RelayCommand PageAppearingCommand => _pageAppearingCommand ??=
+        new RelayCommand(async () => await PageAppearingCommandFunction());
+
+
+        public RelayCommand GetPageAppearingCommand() {
+            if (_recordStorage.IsInitialized()) {
+                return _pageAppearingCommand;
+            } else {
+                return _pageAppearingCommand = new RelayCommand(async () =>
+                    await PageAppearingCommandFunction());
+            }
+        }
+
+        public async Task PageAppearingCommandFunction() {
+            await _recordStorage.InitializeAsync();
+
+            Where = Expression.Lambda<Func<Record, bool>>(
+                Expression.Constant(true),
+                Expression.Parameter(typeof(Record), "p"));
+
+            if (!_newQuery) return;
+            _newQuery = false;
+
+            RecordCollection.Clear();
+            _canLoadMore = true;
+            await RecordCollection.LoadMoreAsync();
+        }
+
+        /// <summary>
+        /// 一页显示的记录数量。
+        /// </summary>
+        public const int PageSize = 800;
+
+        /// <summary>
+        /// 正在载入。
+        /// </summary>
+        public const string Loading = "正在载入";
+
+        /// <summary>
+        /// 没有满足条件的结果。
+        /// </summary>
+        public const string NO_RESULT = "没有满足条件的结果";
+
+        /// <summary>
+        /// 没有更多结果。
+        /// </summary>
+        public const string NO_MORE_RESULT = "没有更多结果";
+
+        // ******** 私有变量
+
+        /// <summary>
+        /// 是否为新查询。
+        /// </summary>
+        private bool _newQuery;
+
+        private bool _canLoadMore = true;
+
+    }
+}
