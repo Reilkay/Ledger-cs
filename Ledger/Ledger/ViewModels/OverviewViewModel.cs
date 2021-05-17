53c670b1516346d1cbb36d195837a0b56447dcd5
Reilkay <zrztt@outlook.com>
2021-05-18 00:59:56 +0800

    feat:完善NewRecordPage布局、OverViewPage支持自定义时间

--- Ledger/Ledger/ViewModels/OverviewViewModel.cs
+++ Ledger/Ledger/ViewModels/OverviewViewModel.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using GalaSoft.MvvmLight.Command;
 using Ledger.Models;
 using Ledger.Services;
+using Xamarin.Forms;
 
 namespace Ledger.ViewModels
 {
@@ -14,6 +15,14 @@ namespace Ledger.ViewModels
         private string _spent;
         private string _balance;
         private List<Record> _records;
+        private DateTime _minDate, _maxDate, _selectedDate;
+
+        public OverviewViewModel()
+        {
+            _minDate = new DateTime(2000, 1, 1);
+            _maxDate = _selectedDate = DateTime.Today;
+            SwitchCommand = new Command(OnDateSelected);
+        }
 
         public string Spent 
         {
@@ -27,6 +36,24 @@ namespace Ledger.ViewModels
             set => SetProperty(ref _balance, value);
         }
 
+        public DateTime MinDate
+        {
+            get => _minDate;
+            set => SetProperty(ref _minDate, value);
+        }
+
+        public DateTime MaxDate
+        {
+            get => _maxDate;
+            set => SetProperty(ref _maxDate, value);
+        }
+
+        public DateTime SelectedDate
+        {
+            get => _selectedDate;
+            set => SetProperty(ref _selectedDate, value);
+        }
+
         /// <summary>
         /// 页面显示命令
         /// </summary>
@@ -36,20 +63,26 @@ namespace Ledger.ViewModels
         /// 页面显示命令
         /// </summary>
         public RelayCommand PageAppearingCommand => _pageAppearingCommand ??=
-            new RelayCommand(async () => await PageAppearingCommandFunction());
+            new RelayCommand(() => PageAppearingCommandFunction());
 
-        public async Task PageAppearingCommandFunction() {
+        public Command SwitchCommand { get; }
+        public void OnDateSelected() => FreshData();
+
+        public void PageAppearingCommandFunction()
+        {
+            _selectedDate = DateTime.Today;
             FreshData();
         }
-
-        private void FreshData() {
+        
+        private void FreshData() 
+        {
             _records = DataStore.GetRecords();
             var _monthIncomeRecords = _records.Where(r =>
-                r.Year == DateTime.Today.Year &&
-                r.Month == DateTime.Today.Month && r.Budget == "收入").ToList();
+                r.Year == _selectedDate.Year &&
+                r.Month == _selectedDate.Month && r.Budget == "收入").ToList();
             var _monthExpensesRecords = _records.Where(r =>
-                r.Year == DateTime.Today.Year &&
-                r.Month == DateTime.Today.Month && r.Budget == "支出").ToList();
+                r.Year == _selectedDate.Year &&
+                r.Month == _selectedDate.Month && r.Budget == "支出").ToList();
             Spent = _monthExpensesRecords.Sum(r => r.Amount).ToString();
             Balance =
                 (_monthIncomeRecords.Sum(r => r.Amount) - float.Parse(Spent))
